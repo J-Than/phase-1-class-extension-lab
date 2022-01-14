2899dedb068415b9fa92a7e456390b5e3ddb8a62
Jarod Wilson <jaroddwilson@gmail.com>
2022-01-14 14:06:12 -0500

    commit tree 6733f78d668955c49beab9f95a4e590fe101f5d9

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+class Polygon {
+  constructor(sidesArray) {
+    this.sidesArray = sidesArray;
+  }
+
+  get countSides() {
+    return this.sidesArray.length;
+  }
+
+  get perimeter() {
+    return this.sidesArray.reduce((p,c) => p+c);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    return this.sidesArray[0] * 2 < this.perimeter && this.sidesArray[1] * 2 < this.perimeter && this.sidesArray[2] * 2 < this.perimeter;
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    return this.sidesArray[0] === this.perimeter / 4 && this.sidesArray[1] === this.perimeter / 4 && this.sidesArray[2] === this.perimeter / 4;
+  }
+
+  get area() {
+    if (this.isValid) {
+      return this.sidesArray[0] * this.sidesArray[1];
+    }
+    return 'Not a square.'
+  }
+}
